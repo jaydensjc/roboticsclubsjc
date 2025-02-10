277e7d0520f78ca31091e48471c47c27ea1ef16c
jaydensjc <s2020161@sjchk.edu.hk>
2025-02-10 21:42:31 +0800

    Add files via upload

---
+++ Face Recognition.py
@@ -0,0 +1,39 @@
+import serial
+import time
+import numpy as np
+import cv2
+
+arduinoData = serial.Serial('COM6', 9600) #Change this based on your serial port
+#try:
+    #arduinoData = serial.Serial('COM14', 9600, timeout=1)  # Update if necessary
+    #print("Port opened successfully!")
+#except serial.SerialException as e:
+    #print(f"Error opening serial port: {e}")
+    #exit()
+
+
+def send_coordinates_to_arduino(x, y, w, h):
+    # Convert the coordinates to a string and send it to Arduino
+    coordinates = f"{x},{y}\r"
+    arduinoData.write(coordinates.encode())
+    print(f"X{x}Y{y}\n")
+
+capture = cv2.VideoCapture(0) #Change the number for the camera that you are using, 0 is for the internal laptop camera, 1 is for an external webcam
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+while True:
+    isTrue, frame = capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.05, 8, minSize=(120,120))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
+        send_coordinates_to_arduino(x, y, w, h)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(200) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
